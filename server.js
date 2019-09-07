bc7ee43d8f07166c2bf4f3c8ad0c5bf3199368d4
Mike D <michaelp.dwyer@gmail.com>
2019-09-07 10:36:37 -0700

    running local

--- server.js
+++ server.js
@@ -14,14 +14,16 @@ app.use(bodyParser.json());
 // DB Config
 const db = require('./config/keys').mongoURI;
 
+//if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+//};
+
 // Connect to Mongo
 mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:Password123@ds219078.mlab.com:19078/heroku_6kjvbl97")
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-};
+
 
   // Use Routes | '/api/items' refers to the 'items' variable defined above to access the 'items.js' routes file
   app.use('/api/items', items)
