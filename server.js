416228b608d9ab2f002f0888c76f07e3b22eeb9b
Mike D <michaelp.dwyer@gmail.com>
2019-09-07 09:50:00 -0700

    Updated several files, added new node_modules and package-lock.json, other Heroku stuff

--- server.js
+++ server.js
@@ -15,10 +15,13 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 // Connect to Mongo
-mongoose
-  .connect(db)
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:Password123@ds219078.mlab.com:19078/heroku_6kjvbl97")
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+};
 
   // Use Routes | '/api/items' refers to the 'items' variable defined above to access the 'items.js' routes file
   app.use('/api/items', items)
